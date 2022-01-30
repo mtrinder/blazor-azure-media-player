d5417cd66a65a54eb624a7bf23afc23ec00817ec
Mark Trinder <mark.trinder@arinco.com.au>
2022-01-30 14:40:04 +1100

    Init

---
+++ BlazorTestAzurePlayer/Client/wwwroot/js/client.js
@@ -0,0 +1,24 @@
+﻿
+window.playVideo = function (urlStreamingLink) {
+    var myPlayer = amp('vid1', { /* Options */
+            techOrder: ["azureHtml5JS", "flashSS", "html5FairPlayHLS", "silverlightSS", "html5"],
+            "nativeControlsForTouch": false,
+            autoplay: false,
+            controls: true,
+            width: "640",
+            height: "400",
+            poster: ""
+        }, function () {
+            console.log('Good to go!');
+            // add an event listener
+            this.addEventListener('ended', function () {
+                console.log('Finished!');
+            });
+        }
+    );
+
+    myPlayer.src([{
+        src: "" + urlStreamingLink,
+        type: "application/vnd.ms-sstr+xml"
+    }]);
+}
